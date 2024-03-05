84ab0634abb656671802ce461a9da079c181c23b
lincxx <bossc@BOSSCO>
2024-03-05 16:15:55 -0500

    Mongoose Info, refactor connect

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+
 const express = require('express')
 const app = express()
 
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
 
 //port for server
 const port = 3000
@@ -25,6 +29,17 @@ app.get('/hello', (req, res) => {
 // app.patch('/api/v1/tasks/:id')    - update task
 // app.delete('/api/v1/tasks/:id')   - delete task
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
-})
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
