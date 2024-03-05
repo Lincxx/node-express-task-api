64b38cf29f9d44b1af12cb5494d1c28b8c24cd7d
lincxx <bossc@BOSSCO>
2024-03-05 06:51:01 -0500

    General Routes, Route Structure and Details

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/hello', (req, res) => {
     res.send("we made it")
 })
 
+
+
+// app.get('/api/v1/tasks')          - get all tasks
+// app.post('/api/v1/tasks')         - create new task
+// app.get('/api/v1/tasks/:id')      - get single task
+// app.patch('/api/v1/tasks/:id')    - update task
+// app.delete('/api/v1/tasks/:id')   - delete task
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
