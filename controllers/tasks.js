effb21024c918bd892cae2b0a7cf252db8382357
lincxx <bossc@BOSSCO>
2024-03-06 08:21:56 -0500

    Get Single Task Controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -21,8 +21,19 @@ const createTask = async (req, res) => {
 
 }
 
-const getTask = (req, res) => {
-    res.json({id:req.params.id})
+const getTask = async (req, res) => {
+    try {
+        //this is how you set an alias
+        const {id:taskID} = req.params
+
+        const task = await Task.findOne({_id:taskID})
+        if(!task) {
+            return res.status(404).json({message: `No task with id:${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch(err) {
+        res.status(500).json({message:err.message})
+    }
 }
 
 const updateTask = (req, res) => {
